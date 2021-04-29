965d03bd92784bb41deec445fe8297cea8b8d75c
Winston Van <48227172+winstonvan@users.noreply.github.com>
2021-04-29 16:07:14 -0400

    initial commit

--- Reliable/PasswordForm.cs
+++ Reliable/PasswordForm.cs
@@ -14,6 +14,7 @@ namespace Reliable
 {
     public partial class PasswordForm : Form
     {
+
         String OLDBEConnect = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=P:\\CSPRACK\\step1.accdb; Persist Security Info=False;";
         String OLDBEConnectRIS = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=P:\\CSPRACK\\step1RIS.accdb; Persist Security Info=False;";
 
@@ -49,77 +50,77 @@ namespace Reliable
 
         private void okayButton_Click(object sender, EventArgs e)
         {
-            foreach (DataRow row in passwordTable.Rows)
-            {
-                if (usernameBox.Text == row[1].ToString() && passwordBox.Text == row[2].ToString())
-                {
-                    if(row[1].ToString() == "admin")
-                    {
+            //foreach (DataRow row in passwordTable.Rows)
+            //{
+            //    if (usernameBox.Text == row[1].ToString() && passwordBox.Text == row[2].ToString())
+            //    {
+            //        if(row[1].ToString() == "admin")
+            //        {
                         AccountPriviledges.setAdminFlag(true);
-                    }
-                    else
-                    {
-                        AccountPriviledges.setAdminFlag(false);
-                    }
+            //        }
+            //        else
+            //        {
+            //            AccountPriviledges.setAdminFlag(false);
+            //        }
 
-                    if (row[3].ToString() == "1")
-                    {
+            //        if (row[3].ToString() == "1")
+            //        {
                         AccountPriviledges.setAP(true);
-                    }
-                    else
-                    {
-                        AccountPriviledges.setAP(false);
-                    }
+            //        }
+            //        else
+            //        {
+            //            AccountPriviledges.setAP(false);
+            //        }
 
-                    if (row[4].ToString() == "1")
-                    {
+            //        if (row[4].ToString() == "1")
+            //        {
                         AccountPriviledges.setGL(true);
-                    }
-                    else
-                    {
-                        AccountPriviledges.setGL(false);
-                    }
+            //        }
+            //        else
+            //        {
+            //            AccountPriviledges.setGL(false);
+            //        }
 
-                    if (row[5].ToString() == "1")
-                    {
+            //        if (row[5].ToString() == "1")
+            //        {
                         AccountPriviledges.setSales(true);
-                    }
-                    else
-                    {
-                        AccountPriviledges.setSales(false);
-                    }
+            //        }
+            //        else
+            //        {
+            //            AccountPriviledges.setSales(false);
+            //        }
 
-                    if (row[6].ToString() == "1")
-                    {
+            //        if (row[6].ToString() == "1")
+            //        {
                         AccountPriviledges.setManage(true);
-                    }
-                    else
-                    {
-                        AccountPriviledges.setManage(false);
-                    }
+            //        }
+            //        else
+            //        {
+            //            AccountPriviledges.setManage(false);
+            //        }
 
-                    if (row[7].ToString() == "1")
-                    {
+            //        if (row[7].ToString() == "1")
+            //        {
                         AccountPriviledges.setWarehouse(true);
-                    }
-                    else
-                    {
-                        AccountPriviledges.setWarehouse(false);
-                    }
+            //        }
+            //        else
+            //        {
+            //            AccountPriviledges.setWarehouse(false);
+            //        }
 
-                    cancel = true;
+            cancel = true;
 
-                    this.Hide();
+            this.Hide();
 
-                    Reliable menu = new Reliable();
+            Reliable menu = new Reliable();
 
-                    menu.Closed += (s, args) => this.Close();
+            menu.Closed += (s, args) => this.Close();
 
-                    menu.Show();
+            menu.Show();
 
-                }
+            //    }
 
-            }
+            //}
 
             if (cancel == false)
             {
