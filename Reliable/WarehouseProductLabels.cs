e403d5e6cd978892cfe2565ee61d25fc0ebbe0b9
wvan <wvan@laurentian.ca>
2022-04-28 09:05:51 -0400

    label fixes

--- Reliable/WarehouseProductLabels.cs
+++ Reliable/WarehouseProductLabels.cs
@@ -238,7 +238,7 @@ namespace Reliable
 
             BaseFont myFontBold = FontFactory.GetFont(BaseFont.HELVETICA_BOLD).BaseFont;
             BaseFont myFontRegular = FontFactory.GetFont(BaseFont.HELVETICA).BaseFont;
-            BaseFont codeFont = BaseFont.CreateFont("c:\\windows\\fonts\\code128.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            BaseFont codeFont = BaseFont.CreateFont("C:\\Windows\\Fonts\\code128.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
 
             int i = 0;
 
